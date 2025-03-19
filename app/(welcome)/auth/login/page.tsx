43ed64ba65ff3f5edb1e10e708537f81b4f03ea1
Titosy Manankasina <dev.titosy@gmail.com>
2025-03-19 15:37:20 +0300

    fix: clean up formatting in LoginPreview component

--- app/(welcome)/auth/login/page.tsx
+++ app/(welcome)/auth/login/page.tsx
@@ -1,12 +1,9 @@
-
-
 import { SignIn } from "@clerk/nextjs";
 
 export default function LoginPreview() {
-
   return (
     <div className="flex h-full min-h-[50vh] w-full flex-col items-center justify-center px-4">
-      <SignIn signUpForceRedirectUrl={"/home"} routing="hash"/>
+      <SignIn signUpForceRedirectUrl={"/home"} routing="hash" />
     </div>
   );
 }
