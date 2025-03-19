0128347e8ef4350b9c9acef01e6bd828fe25d072
Ony <onyrakoto27@gmail.com>
2025-03-19 14:57:54 +0300

    feat: remove learner label from user display in layout component

--- lib/actions.ts
+++ lib/actions.ts
@@ -4,6 +4,8 @@ import { CreateQuizSchema } from "@/schemas/quizSchema";
 import { supabase } from "@/lib/supabase";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import {z} from "zod";
+import {AnswerSchema} from "@/schemas/answerSchema";
 
 export type State = {
   errors?: {
@@ -79,3 +81,23 @@ export async function createQuiz(
     };
   }
 }
+
+
+export async function getAnswers(questionId: string) {
+  const { data, error } = await supabase
+    .from("answers")
+    .select("*")
+    .eq("question_id", questionId);
+
+  if (error) throw new Error(error.message);
+  const validatedData = z.array(AnswerSchema).safeParse(data);
+
+  if (!validatedData.success) {
+    console.error(validatedData.error);
+
+    throw new Error("Invalid data returned from database");
+  }
+
+  return validatedData.data;
+}
+
