f4bfc0947e080e19197518674328511f3abec4a1
Ony <onyrakoto27@gmail.com>
2025-03-21 18:02:29 +0300

    refactor: improve layout structure and styling for header and section components

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -25,81 +25,81 @@ export default function Layout({
 
   return (
     <div className="container mx-auto flex h-screen flex-col px-4">
-      <header className="bg-background flex items-center justify-between border-b py-4 sm:p-6">
-        <div aria-labelledby="logo" className="flex items-center gap-1">
-          <Logo />
+      <div className="sticky top-0 bg-background">
+        <header className="flex items-center justify-between border-b py-4 sm:p-6">
+          <div aria-labelledby="logo" className="flex items-center gap-1">
+            <Logo />
 
-          <Link href="/" className="text-lg font-bold sm:text-2xl">
-            knowsay
-          </Link>
-        </div>
-        <nav className="flex items-center gap-2 md:gap-4">
-          <LanguageSwitch />
-          <div className="hidden md:flex">
-            <AccountMenu />
+            <Link href="/" className="text-lg font-bold sm:text-2xl">
+              knowsay
+            </Link>
           </div>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="rounded-full md:hidden"
-          >
-            <Menu />
+          <nav className="flex items-center gap-2 md:gap-4">
+            <LanguageSwitch />
+            <div className="hidden md:flex">
+              <AccountMenu />
+            </div>
+            <Button
+              variant="ghost"
+              size="icon"
+              className="rounded-full md:hidden"
+            >
+              <Menu />
+            </Button>
+          </nav>
+        </header>
+
+        <section className="bg-neutral-50-50 flex items-center justify-between py-4">
+          {!isAtHomeRoot && <HomeBreadcrumb />}
+
+          <Button variant="link" className="flex items-center gap-2 px-6">
+            <Avatar>
+              <AvatarImage
+                src={
+                  user?.imageUrl ??
+                  "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
+                }
+              />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col items-center justify-center">
+              <span className="text-accent-foreground text-sm font-medium md:inline-block">
+                {user?.firstName}
+              </span>
+            </div>
           </Button>
-        </nav>
-      </header>
 
-      <section className="bg-neutral-50-50 flex items-center justify-between py-4">
-        {!isAtHomeRoot && <HomeBreadcrumb />}
+          {isAtHomeRoot && (
+            <div className="flex gap-4 px-6">
+              <Button
+                asChild
+                variant="outline"
+                className="border-2 border-dashed border-neutral-300 p-4 hover:cursor-pointer"
+              >
+                <Link href="/home/quiz/create">
+                  {t("home.toolbar.buttons.createQuiz")}
+                </Link>
+              </Button>
+              <Button
+                variant="outline"
+                className="hover:cursor-pointe border-2 border-dashed border-neutral-300 p-4"
+              >
+                {t("home.toolbar.buttons.createChallenge")}
+              </Button>
+              <Button
+                variant="outline"
+                className="border-neutral-50-300 border-2 border-dashed p-4 hover:cursor-pointer"
+              >
+                {t("home.toolbar.buttons.createTest")}
+              </Button>
+            </div>
+          )}
+        </section>
 
-        <Button variant="link" className="flex items-center gap-2 px-6">
-          <Avatar>
-            <AvatarImage
-              src={
-                user?.imageUrl ??
-                "https://avataaars.io/?avatarStyle=Circle&topType=LongHairStraight&accessoriesType=Blank&hairColor=BrownDark&facialHairType=Blank&clotheType=BlazerShirt&eyeType=Default&eyebrowType=Default&mouthType=Default&skinColor=Light"
-              }
-            />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-          <div className="flex flex-col items-center justify-center">
-            <span className="text-accent-foreground text-sm font-medium md:inline-block">
-              {user?.firstName}
-            </span>
-          </div>
-        </Button>
-
-        {isAtHomeRoot && (
-          <div className="flex gap-4 px-6">
-            <Button
-              asChild
-              variant="outline"
-              className="border-2 border-dashed border-neutral-300 p-4 hover:cursor-pointer"
-            >
-              <Link href="/home/quiz/create">
-                {t("home.toolbar.buttons.createQuiz")}
-              </Link>
-            </Button>
-            <Button
-              variant="outline"
-              className="hover:cursor-pointe border-2 border-dashed border-neutral-300 p-4"
-            >
-              {t("home.toolbar.buttons.createChallenge")}
-            </Button>
-            <Button
-              variant="outline"
-              className="border-neutral-50-300 border-2 border-dashed p-4 hover:cursor-pointer"
-            >
-              {t("home.toolbar.buttons.createTest")}
-            </Button>
-          </div>
-        )}
-      </section>
-
-      <Separator />
-
-      <div className="h-full w-full overflow-y-auto py-8 pr-3">
-        {children}
+        <Separator />
       </div>
+
+      <div className="w-full py-8 pr-3">{children}</div>
     </div>
   );
 }
