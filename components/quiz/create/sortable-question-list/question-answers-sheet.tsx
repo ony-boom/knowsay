216e56b7b6126b44868e9252da634cd7cfb784e1
Ony <onyrakoto27@gmail.com>
2025-03-22 10:02:24 +0300

    bug: fix build

--- components/quiz/create/sortable-question-list/question-answers-sheet.tsx
+++ components/quiz/create/sortable-question-list/question-answers-sheet.tsx
@@ -1,20 +1,16 @@
 import { DialogTitle } from "@/components/ui/dialog";
 import { SortableQuestionsSchema } from "../sortable-question-list";
-import { AnswerItem } from "./answer-item";
 
 interface QuestionAnswersSheetProps {
-  question: SortableQuestionsSchema["questions"][0];
+  question: SortableQuestionsSchema["questions"][number];
 }
 
 export const QuestionAnswersSheet: React.FC<QuestionAnswersSheetProps> = ({
   question,
 }) => (
   <div className="mt-6 space-y-2">
-    <DialogTitle>{question.question}</DialogTitle>
+    <DialogTitle>{question.content}</DialogTitle>
     <div className="mt-6 grid gap-1.5">
-      {question.answers.map((answer, idx) => (
-        <AnswerItem key={answer.id} answer={answer} index={idx} />
-      ))}
     </div>
   </div>
 );
