8871c35b2938554e06b1806c8ac55ab11ad81ad4
Titosy Manankasina <dev.titosy@gmail.com>
2025-03-24 08:36:03 +0300

    feat: update QuestionInfo component to display the first content text from question data

--- components/quiz/create/sortable-question-list/question-info.tsx
+++ components/quiz/create/sortable-question-list/question-info.tsx
@@ -4,18 +4,29 @@ interface QuestionInfoProps {
   question: SortableQuestionsSchema["questions"][0];
   index: number;
 }
+interface FirstContent {
+  type: "text";
+  text: string;
+}
 
 export const QuestionInfo: React.FC<QuestionInfoProps> = ({
   question,
   index,
-}) => (
-  <div className="flex min-w-0 flex-1 items-center gap-2">
-    <div className="bg-primary/10 text-primary flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-xs font-medium">
-      {index + 1}
+}) => {
+  const questionContent = JSON.parse(question.content);
+  const firstContent = questionContent.at(0)?.content[0] as FirstContent;
+
+  return (
+    <div className="flex min-w-0 flex-1 items-center gap-2">
+      <div className="bg-primary/10 text-primary flex h-6 w-6 shrink-0 items-center justify-center rounded-full text-xs font-medium">
+        {index + 1}
+      </div>
+      <h4 className="line-clamp-1 text-sm font-medium">
+        {firstContent.text ?? ""}
+      </h4>
+      <div className="text-muted-foreground ml-1 shrink-0 text-xs">
+        {question.type}
+      </div>
     </div>
-    <h4 className="line-clamp-1 text-sm font-medium">{question.id}</h4>
-    <div className="text-muted-foreground ml-1 shrink-0 text-xs">
-      {question.type}
-    </div>
-  </div>
-);
+  );
+};
