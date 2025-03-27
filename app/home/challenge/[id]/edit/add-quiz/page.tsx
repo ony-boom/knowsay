33f22eb8dad0b55a9b9706cfb050bce53d54dc4e
Titosy Manankasina <dev.titosy@gmail.com>
2025-03-27 10:17:04 +0300

    feat: enhance quiz creation UI with improved link component and icon

--- app/home/challenge/[id]/edit/add-quiz/page.tsx
+++ app/home/challenge/[id]/edit/add-quiz/page.tsx
@@ -6,6 +6,8 @@ import { QuizListSkeleton } from "@/components/quiz/quiz-list-skeleton";
 import { SearchInput } from "@/components/search";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { getQuizzes } from "@/lib/actions/get-quiz";
+import { Plus } from "lucide-react";
+import Link from "next/link";
 import { Suspense } from "react";
 
 export default async function Page(props: {
@@ -35,26 +37,13 @@ export default async function Page(props: {
           </p>
         </hgroup>
 
-        <a
+        <Link
           href="/quiz/create"
           className="bg-primary text-primary-foreground ring-offset-background hover:bg-primary/90 focus-visible:ring-ring mt-2 inline-flex h-10 items-center justify-center rounded-md px-4 py-2 text-sm font-medium transition-colors focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-50 md:mt-0"
         >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="16"
-            height="16"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            className="mr-2"
-          >
-            <path d="M12 5v14M5 12h14" />
-          </svg>
+          <Plus className="mr-2 h-4 w-4" />
           Create your own quiz
-        </a>
+        </Link>
       </div>
 
       <div className="flex w-full flex-col gap-6 md:flex-row">
