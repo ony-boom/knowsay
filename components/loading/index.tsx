ccd4348e1273903c918e66ee403af56cc3b0c8f0
Ony <onyrakoto27@gmail.com>
2025-04-03 11:35:17 +0300

    Remove Lottie dependency and simplify LoaderProps type

--- components/loading/index.tsx
+++ components/loading/index.tsx
@@ -1,11 +1,7 @@
-import Lottie from "lottie-react";
 import { DynamicLoader } from "./dynamic-loader";
 
 export function GlobalLoading(props: LoaderProps) {
   return <DynamicLoader {...props} />;
 }
 
-export type LoaderProps = Omit<
-  React.ComponentProps<typeof Lottie>,
-  "animationData" | "loop"
->;
+export type LoaderProps = React.ComponentProps<typeof DynamicLoader>;
